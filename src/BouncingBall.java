b913e12f4a96a895785bba8f93d7350c6aa63242
Naya <qwwsoul@gmail.com>
2020-05-25 20:17:18 +0300

    paint, run methods

--- src/BouncingBall.java
+++ src/BouncingBall.java
@@ -31,28 +31,71 @@ public class BouncingBall implements Runnable {
 
         this.field = field;
 
-        radius = new Double(Math.random() * (MAX_RADIUS - MIN_RADIUS)).intValue() + MIN_RADIUS;
+        radius = new Double(Math.random()*(MAX_RADIUS - MIN_RADIUS)).intValue() + MIN_RADIUS;
 
-        speed = new Double(Math.round(5 * MAX_SPEED / radius)).intValue();
-        if (speed > MAX_SPEED) {
+        speed = new Double(Math.round(5*MAX_SPEED / radius)).intValue();
+        if (speed>MAX_SPEED) {
             speed = MAX_SPEED;
         }
 
-        double angle = Math.random() * 2 * Math.PI;
+        double angle = Math.random()*2*Math.PI;
 
-        speedX = 3 * Math.cos(angle);
-        speedY = 3 * Math.sin(angle);
+        speedX = 3*Math.cos(angle);
+        speedY = 3*Math.sin(angle);
 
-        color = new Color((float) Math.random(), (float) Math.random(), (float) Math.random());
+        color = new Color((float)Math.random(), (float)Math.random(), (float)Math.random());
 
-        x = Math.random() * (field.getSize().getWidth() - 2 * radius) + radius;
-        y = Math.random() * (field.getSize().getHeight() - 2 * radius) + radius;
+        x = Math.random()*(field.getSize().getWidth()-2*radius) + radius;
+        y = Math.random()*(field.getSize().getHeight()-2*radius) + radius;
         Thread thisThread = new Thread(this);
         thisThread.start();
     }
 
-    public int getRadius() {
+    public int getRadius(){
         return radius;
 
+    }
+
+    public void run() {
+        try {
+            while(true) {
+                field.canMove(this);
+                if (x + speedX <= radius) {
+                    speedX = -speedX;
+                    x = radius;
+                }
+                else if (x + speedX >= field.getWidth() - radius) {
+                    speedX = -speedX;
+                    x=new Double(field.getWidth()-radius).intValue();
+                }
+                else if (y + speedY <= radius) {
+                    speedY = -speedY;
+                    y = radius;
+                }
+                else if (y + speedY >= field.getHeight() - radius) {
+                    speedY = -speedY;
+                    y=new Double(field.getHeight()-radius).intValue();
+                }
+                else {
+
+                    x += speedX;
+                    y += speedY;
+                }
+
+
+                Thread.sleep(16-speed);
+            }
+        } catch (InterruptedException ex) {
+
+        }
+    }
+
+
+    public void paint(Graphics2D canvas) {
+        canvas.setColor(color);
+        canvas.setPaint(color);
+        Ellipse2D.Double ball = new Ellipse2D.Double(x-radius, y-radius, 2*radius, 2*radius);
+        canvas.draw(ball);
+        canvas.fill(ball);
     }
 }
