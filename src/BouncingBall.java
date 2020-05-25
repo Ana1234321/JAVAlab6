c4e8c6059785fd062d45f49e429b72ed8c1e64ad
Naya <qwwsoul@gmail.com>
2020-05-25 20:15:43 +0300

    BouncingBall setup: vars, getters, constructor(BB)

---
+++ src/BouncingBall.java
@@ -0,0 +1,58 @@
+
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+
+public class BouncingBall implements Runnable {
+
+    // Максимальный радиус, который может иметь мяч
+    private static final int MAX_RADIUS = 40;
+    // Минимальный радиус, который может иметь мяч
+    private static final int MIN_RADIUS = 3;
+    // Максимальная скорость, с которой может летать мяч
+    private static final int MAX_SPEED = 15;
+
+    private Field field;
+    private int radius;
+    private Color color;
+
+    // Текущие координаты мяча
+    private double x;
+    private double y;
+
+    // Вертикальная и горизонтальная компонента скорости
+    private int speed;
+    private double speedX;
+    private double speedY;
+
+    public BouncingBall(Field field) {
+
+
+        this.field = field;
+
+        radius = new Double(Math.random() * (MAX_RADIUS - MIN_RADIUS)).intValue() + MIN_RADIUS;
+
+        speed = new Double(Math.round(5 * MAX_SPEED / radius)).intValue();
+        if (speed > MAX_SPEED) {
+            speed = MAX_SPEED;
+        }
+
+        double angle = Math.random() * 2 * Math.PI;
+
+        speedX = 3 * Math.cos(angle);
+        speedY = 3 * Math.sin(angle);
+
+        color = new Color((float) Math.random(), (float) Math.random(), (float) Math.random());
+
+        x = Math.random() * (field.getSize().getWidth() - 2 * radius) + radius;
+        y = Math.random() * (field.getSize().getHeight() - 2 * radius) + radius;
+        Thread thisThread = new Thread(this);
+        thisThread.start();
+    }
+
+    public int getRadius() {
+        return radius;
+
+    }
+}
